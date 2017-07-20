8430d9044ffb4a1986f6d5af262f2db62518eae9
alf <alfmohenu@outlook.com>
2017-07-20 10:59:08 +0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,16 @@
+const {BrowserWindow, app} = require("electron")
+const path = require("path")
+const url = require("url")
+
+let window
+
+function createWindow(){
+  window = new BrowserWindow({width: 800, height: 600})
+  window.loadURL(url.format({
+    pathname: path.join(__dirname, "index.html"),
+    protocol: "file:",
+    slashes: true
+  }))
+}
+
+app.on("ready", createWindow)
