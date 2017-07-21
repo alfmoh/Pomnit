e8eb909b36391d422c1c00660365fa53881f6636
alf <alfmohenu@outlook.com>
2017-07-21 14:59:10 +0300

    Notyf added. Button click event added

--- renderer.js
+++ renderer.js
@@ -2,11 +2,25 @@ let $ = require("jquery")
 //require("bootstrap-datepicker")
 let Kalendae = require("kalendae")
 let datetimepicker = require("eonasdan-bootstrap-datetimepicker")
+let notyf = require("notyf")
 
 new Kalendae.Input("date",{});
  
+let note = new notyf()
+
 $("#time").datetimepicker({
     format: "LT"
 })
 
 
+$("#add-button").on("click", ()=>{
+    let value = $("#event-title").val()
+    if (value.trim() !== "") {
+     $("#event-list").append("<li class='list-group-item'>"+value +"</li>") 
+       note.confirm("Added")
+    }  
+    else{
+        
+        note.alert("Hey, you left something out")
+    }  
+})
